4ad70989da6bf68892153b3d59bbfc37c5ce4efe
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-09-19 16:26:37 -0400

    chore: adicionado endpoint e getters para atualização de classe

--- src/stores/endpoints.js
+++ src/stores/endpoints.js
@@ -37,6 +37,7 @@ export const useEndpoints = defineStore('endpoints', {
         listagem: `${servidor}/api/v1/classe_produto/listagem`,
         cadastro: `${servidor}/api/v1/classe_produto/cadastro`,
         consulta: `${servidor}/api/v1/classe_produto/classe/`,
+        edicao: `${servidor}/api/v1/classe_produto/classe/`,
       }
     },
     token: `Bearer ${localStorage.getItem('token') || sessionStorage.getItem('token')}`
@@ -60,5 +61,6 @@ export const useEndpoints = defineStore('endpoints', {
     getListagemClasseProduto: (state) => state.estoque.classes.listagem,
     getCadastroClasseProduto: (state) => state.estoque.classes.cadastro,
     getConsultaClasseProduto: (state) => state.estoque.classes.consulta,
+    getEdicaoClasseProduto: (state) => state.estoque.classes.edicao,
   }
 })
